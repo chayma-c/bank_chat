313a690e112852e6f30da3bd0b2790e86d4b41a2
chayma <chayma.sansa@gmail.com>
2026-04-08 13:12:49 +0100

    90% of api calls work in role management

---
+++ backend/chatbot/auth/settings.py
@@ -0,0 +1,14 @@
+# Keycloak settings
+KEYCLOAK_URL = env('KEYCLOAK_URL', default='http://localhost:8080')
+KEYCLOAK_REALM = env('KEYCLOAK_REALM', default='my-realm')
+KEYCLOAK_CLIENT_ID = env('KEYCLOAK_CLIENT_ID', default='bank_chat')
+
+# REST Framework settings
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'path.to.authentication.KeycloakJWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+}
